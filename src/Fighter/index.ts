5cef78111803655aa17ba60262e9e46745cfafcc
Pablo Dominguez <pasaxo@gmail.com>
2023-05-08 17:09:16 -0300

    implementação interface SimpleFigther

--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,7 +1,7 @@
 import Fighter from './Fighter';
-// import SimpleFighter from './SimpleFighter';
+import SimpleFighter from './SimpleFighter';
 
 export default Fighter;
-// export { 
-//   SimpleFighter, 
-// };
+export { 
+  SimpleFighter, 
+};
